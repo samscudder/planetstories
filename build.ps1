6738ad0dc976b285aed7dc8b0bf721de346074e5
Simon Scudder <simon.scudder@metaltoad.com>
2025-09-17 15:04:50 -0300

    Updated stories

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 Remove-Item *.epub
 Set-Location -path .\v01n01
-# 7z a "Planet Stories V01N01.epub" -mx=0 `#mimetype
-# 7z a "Planet Stories V01N01.epub" META-INF OEBPS
-# 7z rn "Planet Stories V01N01.epub" `#mimetype mimetype
-# Move-Item -Path "Planet Stories V01N01.epub" -Destination .. -Force
+7z a "Planet Stories V01N01.epub" -mx=0 `#mimetype
+7z a "Planet Stories V01N01.epub" META-INF OEBPS
+7z rn "Planet Stories V01N01.epub" `#mimetype mimetype
+Move-Item -Path "Planet Stories V01N01.epub" -Destination .. -Force
 Set-Location -path ..\v01n02
 7z a "Planet Stories V01N02.epub" -mx=0 `#mimetype
 7z a "Planet Stories V01N02.epub" META-INF OEBPS
@@ -19,16 +19,16 @@ Set-Location -path ..\v01n04
 7z a "Planet Stories V01N04.epub" META-INF OEBPS
 7z rn "Planet Stories V01N04.epub" `#mimetype mimetype
 Move-Item -Path "Planet Stories V01N04.epub" -Destination .. -Force
-# Set-Location -path ..\v01n05
-# 7z a "Planet Stories V01N05.epub" -mx=0 `#mimetype
-# 7z a "Planet Stories V01N05.epub" META-INF OEBPS
-# 7z rn "Planet Stories V01N05.epub" `#mimetype mimetype
-# Move-Item -Path "Planet Stories V01N05.epub" -Destination .. -Force
-# Set-Location -path ..\v01n06
-# 7z a "Planet Stories V01N06.epub" -mx=0 `#mimetype
-# 7z a "Planet Stories V01N06.epub" META-INF OEBPS
-# 7z rn "Planet Stories V01N06.epub" `#mimetype mimetype
-# Move-Item -Path "Planet Stories V01N06.epub" -Destination .. -Force
+Set-Location -path ..\v01n05
+7z a "Planet Stories V01N05.epub" -mx=0 `#mimetype
+7z a "Planet Stories V01N05.epub" META-INF OEBPS
+7z rn "Planet Stories V01N05.epub" `#mimetype mimetype
+Move-Item -Path "Planet Stories V01N05.epub" -Destination .. -Force
+Set-Location -path ..\v01n06
+7z a "Planet Stories V01N06.epub" -mx=0 `#mimetype
+7z a "Planet Stories V01N06.epub" META-INF OEBPS
+7z rn "Planet Stories V01N06.epub" `#mimetype mimetype
+Move-Item -Path "Planet Stories V01N06.epub" -Destination .. -Force
 # Set-Location -path ..\v01n07
 # 7z a "Planet Stories V01N07.epub" -mx=0 `#mimetype
 # 7z a "Planet Stories V01N07.epub" META-INF OEBPS
@@ -61,18 +61,18 @@ Move-Item -Path "Planet Stories V01N04.epub" -Destination .. -Force
 # Move-Item -Path "Planet Stories V01N12.epub" -Destination .. -Force
 Set-Location -path ..
 python \Library\Incoming\epub_proof.py
-# Write-Output 'Planet Stories V01N01.epub'
-# java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N01.epub'
-# Write-Output 'Planet Stories V01N02.epub'
-# java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N02.epub'
-# Write-Output 'Planet Stories V01N03.epub'
-# java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N03.epub'
+Write-Output 'Planet Stories V01N01.epub'
+java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N01.epub'
+Write-Output 'Planet Stories V01N02.epub'
+java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N02.epub'
+Write-Output 'Planet Stories V01N03.epub'
+java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N03.epub'
 Write-Output 'Planet Stories V01N04.epub'
 java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N04.epub'
-# Write-Output 'Planet Stories V01N05.epub'
-# java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N05.epub'
-# Write-Output 'Planet Stories V01N06.epub'
-# java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N06.epub'
+Write-Output 'Planet Stories V01N05.epub'
+java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N05.epub'
+Write-Output 'Planet Stories V01N06.epub'
+java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N06.epub'
 # Write-Output 'Planet Stories V01N07.epub'
 # java -Xss1024K -jar D:\epubcheck-5.1.0\epubcheck.jar 'Planet Stories V01N07.epub'
 # Write-Output 'Planet Stories V01N08.epub'
